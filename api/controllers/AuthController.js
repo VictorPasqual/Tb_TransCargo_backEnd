333e56481eddae570ac6f2a357ce178a0967b995
Victor Pasqual <vhbp2003@gmail.com>
2023-04-14 16:25:27 -0300

    Consertei o bug do cors

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -1,40 +1,39 @@
-// controllers/authController.js
 const { Users } = require('../models');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 const { JWT_SECRET, JWT_EXPIRATION_TIME } = require('../config/configJwt');
 
 module.exports = {
     async authenticate(req, res) {
         const { email, password } = req.body;
+        console.log(email, password)
         try {
             // Verifique se o usuário existe e se a senha está correta
             const user = await Users.findOne({ where: { email } });
+            console.log(user)
+
             if (!user) {
                 return res.status(401).json({ message: 'Credenciais inválidas.' });
             }
-            const isPasswordValid = await bcrypt.compare(password, user.password);
 
-            if (!isPasswordValid) {
-                return res.status(401).json({ message: 'Credenciais inválidas.' });
+            // Verifique se a senha fornecida corresponde à senha armazenada
+
+            const passwordMatch = password === user.password
+
+            if (!passwordMatch) {
+                return res.status(401).json({ message: 'Credenciais inválidas' });
             }
 
-            // Se o usuário existir e a senha estiver correta, retorne um token JWT para o frontend
-            const token = jwt.sign({ id: user.id }, JWT_SECRET, { expiresIn: JWT_EXPIRATION_TIME });
-            console.log(token)
-            return res.status(200).json({ userId: user.id, token });
+            // Se chegamos aqui, o usuário está autenticado
+            const token = jwt.sign({ id: user.id }, JWT_SECRET, {
+                expiresIn: JWT_EXPIRATION_TIME,
+            });
+
+            res.json({ token });
         } catch (err) {
             console.error(err);
             return res.status(500).json({ message: 'Erro interno do servidor.' });
         }
     },
 
-    async validateUserRole(req, res, next) {
-        const userRole = req.user.role;
-        if (userRole === 'admin' || userRole === 'motorista' || userRole === 'cliente') {
-            next();
-        } else {
-            res.status(403).json({ message: 'Você não tem permissão para acessar esta rota.' });
-        }
-    }
 };
--- api/server.js
+++ api/server.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const routes = require('./routes')
-
-routes(app)
+const routes = require('./routes');
 
 app.use(cors());
+
+app.use(express.json()); // Adicionar para permitir o uso do corpo da requisição em JSON
+
+routes(app);
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "*");
-    res.header("Access-Control-Max-Age", "86400")
+    res.header("Access-Control-Max-Age", "86400");
     if (req.method === 'OPTIONS') {
-        res.header('Acess-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET'); // Corrigir nome da função
         return res.status(200).send({});
     }
 
